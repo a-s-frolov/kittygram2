8a3432408ffbad5f1e30ec7acffcb5ff313a3f8c
Андрей Фролов <asfrolov@yandex.ru>
2022-03-18 16:42:13 +0300

    ревизия перед практическим заданием

--- cats/models.py
+++ cats/models.py
@@ -21,11 +21,28 @@ class Achievement(models.Model):
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16, choices=CHOICES)
+    color = models.CharField(
+        max_length=16,
+        choices=CHOICES
+    )
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
-        User, related_name='cats', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+        User,
+        related_name='cats',
+        on_delete=models.CASCADE
+    )
+    achievements = models.ManyToManyField(
+        Achievement,
+        through='AchievementCat'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owner'],
+                name='unique_name_owner'
+            )
+        ]
 
     def __str__(self):
         return self.name
